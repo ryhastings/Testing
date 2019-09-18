553ac6a3d6c06185e5c4f7a8b93477903f0814f3
Ryan Hastings <rjh53@duke.edu>
2019-09-18 13:10:00 -0400

    test

--- test_subtract.py
+++ test_subtract.py
@@ -4,7 +4,8 @@ import pytest
 							(2,3,-1),
 							(3,7,-4),
 							(-2,5,-7),
-							(5,5,0)])
+							(5,5,0),
+							(6,6,0)])
 
 def test_subtract(a,b,expected):
 	from subtract import subtract
